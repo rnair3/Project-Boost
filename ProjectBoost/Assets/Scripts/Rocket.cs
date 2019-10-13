f5f25b1e0d5ca9016bfc9c9d8c8480331aa50cb4
Rinita Nair <rnair3@uncc.edu>
2019-10-13 00:07:58 -0400

    Creating levels and scripts for movement

---
+++ ProjectBoost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    AudioSource audio;
+
+    [SerializeField] float rcsThrust = 250f;
+    [SerializeField] float mainThrust = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();        
+    }
+
+    private void ProcessInput()
+    {
+        Thrust();
+        RotateShip();
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audio.isPlaying)
+                audio.Play();
+        }
+        else
+        {
+            audio.Stop();
+        }
+    }
+
+    private void RotateShip()
+    {
+        rigidbody.freezeRotation = true;    //take manual control of rotation
+        float rotation = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotation);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotation);
+        }
+        rigidbody.freezeRotation = false;   //resume physics control of rotation
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Fuel":
+                break;
+            default:
+                break;
+        }
+    }
+}
